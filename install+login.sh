da49d96d7885d826eae894ed61d62cd4797a390f
ivan silvio <ivan.sl@hotmail.com>
2021-11-27 22:13:23 -0300

    Create install+login.sh

---
+++ install+login.sh
@@ -0,0 +1,27 @@
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+
+
+ibmcloud plugin install auto-scaling
+ibmcloud plugin install container-registry logging
+ibmcloud plugin install container-registry 
+ibmcloud plugin install  logging
+ibmcloud plugin repo-plugins -r "IBM Clou
+ibmcloud plugin install  container-service
+ibmcloud plugin install event-notifications
+ibmcloud plugin install cloud-functions
+ibmcloud plugin install  dbaas-cli
+ibmcloud plugin install cloud-databases
+ibmcloud plugin install doi[
+ibmcloud plugin install doi
+ibmcloud plugin install vpc-infrastructure
+ibmcloud plugin install cloud-dns-services
+ibmcloud plugin install observe-service 
+ibmcloud plugin install code-engine
+ibmcloud plugin install secrets-manager
+ibmcloud plugin install  monitoring
+ 
+ 
+ibmcloud plugin repo-plugins -r "IBM Cloud"
+
+
+ ibmcloud login --sso
